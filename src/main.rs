a04113ddeb6a3a3518905b385ff3f2361a7c7f4e
Deep <deepgauravraj@gmail.com>
2025-04-27 20:22:08 +0000

    rust sdmmc

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,94 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+fn main() -> anyhow::Result<()> {
+    use std::fs::{read_dir, File};
+    use std::io::{Read, Seek, Write};
+
+    use esp_idf_svc::fs::fatfs::Fatfs;
+    use esp_idf_svc::hal::gpio;
+    use esp_idf_svc::hal::prelude::*;
+    use esp_idf_svc::hal::sd::{
+        mmc::SdMmcHostConfiguration, mmc::SdMmcHostDriver, SdCardConfiguration, SdCardDriver,
+    };
+    use esp_idf_svc::io::vfs::MountedFatfs;
+    use esp_idf_svc::log::EspLogger;
+
+    use log::info;
+
     esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take()?;
+    let pins = peripherals.pins;
+
+    let sd_card_driver = SdCardDriver::new_mmc(
+        // => Data width = 4 bits
+        // SdMmcHostDriver::new_slot1_4bits(
+        //     peripherals.sdmmc1,
+        //     pins.gpio15,
+        //     pins.gpio14,
+        //     pins.gpio2,
+        //     pins.gpio4,
+        //     pins.gpio12,
+        //     pins.gpio13,
+        //     None::<gpio::AnyIOPin>,
+        //     None::<gpio::AnyIOPin>,
+        //     &SdMmcHostConfiguration::new(),
+        // )?,
+        // => Data width = 1 bit
+        // Comment out the above configuration and uncomment this block
+        // if you have connected only the d0 pin
+        SdMmcHostDriver::new_1bit(
+            peripherals.sdmmc1,
+            pins.gpio3,
+            pins.gpio1,
+            pins.gpio2,
+            None::<gpio::AnyIOPin>,
+            None::<gpio::AnyIOPin>,
+            &SdMmcHostConfiguration::new(),
+        )?,
+        &SdCardConfiguration::new(),
+    )?;
+
+    // Keep it around or else it will be dropped and unmounted
+    let _mounted_fatfs = MountedFatfs::mount(Fatfs::new_sdcard(0, sd_card_driver)?, "/sdcard", 4)?;
+
+    let content = b"Hello, world!";
+
+    {
+        let mut file = File::create("/sdcard/test.txt")?;
+
+        info!("File {file:?} created");
+
+        file.write_all(content).expect("Write failed");
+
+        info!("File {file:?} written with {content:?}");
+
+        file.seek(std::io::SeekFrom::Start(0)).expect("Seek failed");
+
+        info!("File {file:?} seeked");
+    }
+
+    {
+        let mut file = File::open("/sdcard/test.txt")?;
+
+        info!("File {file:?} opened");
+
+        let mut file_content = String::new();
+
+        file.read_to_string(&mut file_content).expect("Read failed");
+
+        info!("File {file:?} read: {file_content}");
+
+        assert_eq!(file_content.as_bytes(), content);
+    }
+
+    {
+        let directory = read_dir("/sdcard")?;
+
+        for entry in directory {
+            log::info!("Entry: {:?}", entry?.file_name());
+        }
+    }
+
+    Ok(())
 }
